7e3f3e985c8af48afe443b90ea65fc0b23a57a1c
allen-login <jinhanchenn@outlook.com>
2021-05-22 21:08:58 +0800

    py

--- text.py
+++ text.py
@@ -1,17 +1,15 @@
-ls = [1, 3, 2]
-lsw = [3, 3, 3, 3]
-x = [ls, lsw]
-print(x)
-x = list
+
 
 
 class Node:
-    def __init__(self,):
-        self.value = "null"
+    def __init__(self,v):
+        self.value = v
+        if v == None:
+            self.value = "null"
         self.left = "null"
         self.right = "null"
     def set_value(self,k):
-        self.value = "null"
+        self.value = k
 
     def set_left(self, strxx):
         self.left = strxx
@@ -20,9 +18,9 @@ class Node:
         self.right = stryy
 
     def read(self):
-        print(self.value)
-        print(self.left)
-        print(self.right)
+        print("v"+str(self.value))
+        print(str(self.left))
+        print(str(self.right))
     def what(self):
         if not self.left == "null" and not self.right == "null":
             return "all"
@@ -34,57 +32,55 @@ class Node:
             return "no"
 
     def node(self):
-        if self.left == "null" or self.right == "null":
+        if self.left == "null" and self.right == "null":
             print("no")
             return False
-        print("left:"+str(self.left.value))
+        print("t")
+        print("left"+str(self.left.value))
         print("right:"+str(self.right.value))
 
-xc = Node()
+xc = Node("null")
+
 def build(goal,n):
-    # 左小右大
+    if goal.value == "null":
+        goal.value = n
     if goal.what() == "no":
-        if goal.value == "null":
-            goal.set_value(n)
-        if goal.what() == "all":
-            if n < goal.left.value:
-                goal.left.set_left(n)
-            if n > goal.left.value:
-                goal.left.set_right(n)
-            if n < goal.right.value:
-                goal.right.set_left(n)
-            if n > goal.right.value:
-                goal.right.set_right(n)
-        if goal.what() == "left":
-            if goal.left.value < n:
-                goal.set_right(n)
-            if goal.left.value > n:
-                goal.left.set_left(n)
-        if goal.what() == "right":
-            if goal.right.value < n:
-                goal.set_left(n)
-            if goal.right.value > n:
-                goal.right.set_left(n)
-        if goal.what() == "no":
-            if goal.value == "null":
-                goal.set_value(n)
-            if n < goal.value:
-                goal.set_left(n)
-            if n > goal.value:
-                goal.set_right(n)
+        if n < goal.value:
+            goal.left = Node(n)
+        if n > goal.value:
+            goal.right = Node(n)
+    if goal.what() == "all":
+        if n > goal.value:
+            build(goal.right,n)
+        if n < goal.value:
+            build(goal.left,n)
+    if goal.what() == "left":
+        if n < goal.value:
+            goal.left = Node(n)
+        if n > goal.value:
+            goal.right = Node(n)
+    if goal.right == "right":
+        if n > goal.value:
+            build(goal.right,n)
+        if n < goal.value:
+            goal.left = Node(n)
+
+
+if __name__ == '__main__':
+    build(xc, 5)
+    build(xc, 3)
+    build(xc, 7)
+    build(xc, 4)
+
+    xc.read()
+    xc.node()
 
 
 
 
 
 
-build(xc,1)
-build(xc,4)
-build(xc,3)
 
-
-xc.read()
-xc.node()
 
 
 
